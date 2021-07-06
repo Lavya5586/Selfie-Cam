8a06e412d4487ec120866976dba69a06a95088f6
Lavya5586 <78597821+Lavya5586@users.noreply.github.com>
2021-07-06 16:26:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function (event){
+    console.log(event);
+var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    if(Content == "Take My Selfie")
+    {
+        speak();
+        console.log("Taking Selfie!!");
+    }
+}
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+  });
+  camera = document.getElementById("camera");
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data = "Taking Your Selfie In 5 Seconds";
+    var utterThis = SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
+}
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save() 
+{
+    var link = document.getElementById("link");
+    var image = document.getElementById("capture_image");
+    link.href = image;
+    link.click();
+}
